654de4bf3f1f950c5f84839ceaebfd772d721615
bartvs98 <bartvanstraaten98@gmail.com>
2017-07-28 13:33:01 +0200

    First commit after migration

---
+++ bower_components/custom/js/script.js
@@ -0,0 +1,36 @@
+// create module
+var mainApp = angular.module("mainApp", ["ngRoute", "ngAnimate"]);
+
+// configure routes
+mainApp.config(function($routeProvider) {
+  $routeProvider
+
+    // route for home page
+    .when("/", {
+      templateUrl : 'pages/homepage.html',
+      controller  : 'mainController'
+    })
+
+    .when("/404", {
+      templateUrl : "pages/404.html",
+      controller  : "404Controller"
+    })
+
+    .otherwise({ redirectTo: '/404' });
+})
+
+// create controller and inject Angular's $scope
+mainApp.controller("mainController", function($scope, $sce, $location, $anchorScroll) {
+  // create a message to desplay in our view
+  $scope.caption = "PRESENT YOURSELF IN AN UNIQUE WAY";
+
+  $scope.aboutMe = "My name is Bart van Straaten, I am situated in The Netherlands. " +
+                   "Currently I am studying HBO-ICT at the Hogeschool Utrecht. " +
+                   "I just finished the first year of Software And Information Engineering which is the branch I have chosen to develop myself in. " +
+                   "This includes but is not limited to:";
+});
+
+mainApp.controller("404Controller", function($scope) {
+  $scope.message = "Sorry this page does not exist."
+  $scope.title = "Boekhouding | 404";
+})
